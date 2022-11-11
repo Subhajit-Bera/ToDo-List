2f8ef18bc006897dbea318a2a87b57363328da17
Subhajit <subhabera211@gmail.com>
2022-11-11 19:43:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,28 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
+
+//Connect with mongodb database.
+mongoose.connect("mongodb://localhost:27017/toDolistDB");
+
+
+const itemSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    name: "This is your ToDoList"
+})
+
+const item2 = new Item({
+    name: "Hit + for add new items"
+})
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item"
+})
+
+const defaultItems = [item1, item2, item3];
